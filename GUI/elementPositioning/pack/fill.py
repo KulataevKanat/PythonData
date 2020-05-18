4afd49e12e55af6303aeccda44c58c09ec4c620d
KulataevKanat <RaketaEnot4466@gmail.com>
2020-05-18 18:40:41 +0600

    fill

--- GUI/elementPositioning/pack/fill.py
+++ GUI/elementPositioning/pack/fill.py
@@ -11,6 +11,24 @@ btn1 = Button(text="CLICK ME",
               pady="6",
               font="15"
               )
-btn1.pack(fill=BOTH)
+btn1.pack(side=RIGHT, fill=Y)
+
+btn2 = Button(text="!-?",
+              bg="#555",
+              fg="#ccc",
+              padx="15",
+              pady="6",
+              font="15"
+              )
+btn2.pack(side=TOP, fill=X)
+
+btn3 = Button(text="CLICK ME(2)",
+              background="#555",
+              foreground="#ccc",
+              padx="15",
+              pady="6",
+              font="15"
+              )
+btn3.pack(side=RIGHT, fill=Y)
 
 root.mainloop()
