afc926e9aa8f905a4fcd8f4e78dde30c8d7e7502
KulataevKanat <RaketaEnot4466@gmail.com>
2020-05-21 05:21:31 +0600

    checkbutton

---
+++ GUI/elements/checkbutton.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+root = Tk()
+root.title("GUI на Python")
+root.geometry("300x250")
+
+python_lang = IntVar()
+python_checkbutton = Checkbutton(text="Python",
+                                 variable=python_lang,
+                                 onvalue=1,
+                                 offvalue=0,
+                                 padx=15,
+                                 pady=10
+                                 )
+python_checkbutton.grid(row=0,
+                        column=0,
+                        sticky=W
+                        )
+
+java_lang = IntVar()
+java_checkbutton = Checkbutton(text="Java",
+                               variable=java_lang,
+                               onvalue=1,
+                               offvalue=0,
+                               padx=15,
+                               pady=10
+                               )
+java_checkbutton.grid(row=1,
+                      column=0,
+                      sticky=W
+                      )
+
+root.mainloop()
