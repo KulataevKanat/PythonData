d60f6e5e5550ce3194a9e7dfe334c0d3a203fbda
KulataevKanat <RaketaEnot4466@gmail.com>
2020-05-20 06:45:24 +0600

    add methods print for compilation read

--- structures/oop/polymorphism/objects.py
+++ structures/oop/polymorphism/objects.py
@@ -1,11 +1,16 @@
 from oop.polymorphism.classes import Animal, Dog, Cat
 
+print("class Animal: ")
 animal = Animal("animal-name")
 animal.make_a_sound()
 animal.display_info()
+
+print("\nclass Dog: ")
 dog = Dog("Shnaps")
 dog.make_a_sound()
 dog.display_info()
+
+print("\nclass Cat: ")
 cat = Cat("Oleg")
 cat.make_a_sound()
 cat.display_info()
