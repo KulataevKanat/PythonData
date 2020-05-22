4168246cb984f266b53f6f4d06fd898ffeec6c4f
KulataevKanat <RaketaEnot4466@gmail.com>
2020-05-23 01:05:37 +0600

    reformat code

--- GUI/elements/checkbutton.py
+++ GUI/elements/checkbutton.py
@@ -5,29 +5,33 @@ root.title("GUI на Python")
 root.geometry("300x250")
 
 python_lang = IntVar()
-python_checkbutton = Checkbutton(text="Python",
-                                 variable=python_lang,
-                                 onvalue=1,
-                                 offvalue=0,
-                                 padx=15,
-                                 pady=10
-                                 )
-python_checkbutton.grid(row=0,
-                        column=0,
-                        sticky=W
-                        )
+python_checkbutton = Checkbutton(
+    text="Python",
+    variable=python_lang,
+    onvalue=1,
+    offvalue=0,
+    padx=15,
+    pady=10
+)
+python_checkbutton.grid(
+    row=0,
+    column=0,
+    sticky=W
+)
 
 java_lang = IntVar()
-java_checkbutton = Checkbutton(text="Java",
-                               variable=java_lang,
-                               onvalue=1,
-                               offvalue=0,
-                               padx=15,
-                               pady=10
-                               )
-java_checkbutton.grid(row=1,
-                      column=0,
-                      sticky=W
-                      )
+java_checkbutton = Checkbutton(
+    text="Java",
+    variable=java_lang,
+    onvalue=1,
+    offvalue=0,
+    padx=15,
+    pady=10
+)
+java_checkbutton.grid(
+    row=1,
+    column=0,
+    sticky=W
+)
 
 root.mainloop()
