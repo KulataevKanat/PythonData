ec0015a9da014337b4f610f893669c7742931d6e
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-11-12 01:15:27 +0600

    echo bot

---
+++ telegram_bot/echo_bot.py
@@ -0,0 +1,13 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.token)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_msg(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
